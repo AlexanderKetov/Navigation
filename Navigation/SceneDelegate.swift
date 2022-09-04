70669547deaeec22c99cdc3db239f6353ae9a1b8
Alexander Ketov <93509658+AlexanderKetov@users.noreply.github.com>
2022-09-04 20:49:28 +0700

    HomeWork_6.1

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -17,11 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         //guard let _ = (scene as? UIWindowScene) else { return }
         
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let appConfiguration = AppConfiguration.linkOne
+        let networkManager = NetworkManager(baseAddress: appConfiguration.rawValue) //решил передавать стразу мененджеру, а не методу мененджера
+        networkManager.request()
         
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-        //let navigationController = UINavigationController(rootViewController: TabBar())
         let mainCoordinator = MainCoordinator()
         window?.rootViewController = mainCoordinator.startApplication()
         window?.makeKeyAndVisible()
